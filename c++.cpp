312eeeea0ba252f3ce6e7f056f0709c7472f3f2c
unknown <vipsviper2@gmail.com>
2025-09-05 22:47:39 +0530

    initial commit

---
+++ c++.cpp
@@ -0,0 +1,13 @@
+                                    //stl arrays for leetcode amd vector
+#include<iostream>
+#include<stack>
+using namespace std ; 
+
+int main()
+{
+    stack<string>s;
+    s.push("vipul");
+    s.push("shah");
+    cout<<"top element :"<<s.top()<<endl;
+    cout<<"hlo";
+}
